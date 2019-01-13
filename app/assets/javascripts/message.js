350ea414ca40a502006f8d65183694f414b38d6a
矢野 善尊 <yancha.comparr1027@gmail.com>
2019-01-13 21:36:24 +0900

    Addition of Ajax description

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,5 +3,13 @@ $(function() {
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   });
 });
