e235d9e5096cf8d9b69c4df2145f63cba66199c4
矢野 善尊 <yancha.comparr1027@gmail.com>
2019-01-15 00:01:51 +0900

    Implement the function to add / delete chat members after incremental search

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,18 +1,29 @@
 $(function(){
-  var search_list = $("#user-search-result");
+  var searchList = $("#user-search-result");
+  var chatGroupMembers = $("#chat-group-users");
+
   function appendUser(user){
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ user.name }</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${ user.id } data-user-name=${ user.name }>追加</a>
                 </div>`
-    search_list.append(html);
+    searchList.append(html);
   }
 
   function appendNoUser(user){
     var html = `<div class="chat-group-user cleafix">
                   <p class="chat-group-user__name">${ user }</p>
                 </div>`
-    search_list.append(html);
+    searchList.append(html);
+  }
+
+  function appendMember(id, name){
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${ id }'>
+                  <input name='group[user_ids][]' type='hidden' value=${ id }>
+                  <p class='chat-group-user__name'>${ name }</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    chatGroupMembers.append(html);
   }
 
   $("#user-search-field").on("keyup", function(){
@@ -29,7 +40,6 @@ $(function(){
       if (users.length !== 0){
         users.forEach(function(user){
           appendUser(user);
-          console.log();
         });
       }
       else {
@@ -39,5 +49,16 @@ $(function(){
     .fail(function(){
       alert('ユーザー検索に失敗しました');
     })
+  });
+
+  searchList.on("click", '.user-search-add', function(){
+    var id = $(this).data('user-id');
+    var name = $(this).data('user-name');
+    appendMember(id, name);
+    searchList.empty();
+  });
+
+  chatGroupMembers.on("click", '.user-search-remove', function(){
+    $(this).parent().remove();
   });
 });
