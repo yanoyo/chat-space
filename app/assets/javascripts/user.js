1deb150bd719424c684703c3f5fb679a7d939d98
矢野 善尊 <yancha.comparr1027@gmail.com>
2019-01-14 21:19:44 +0900

    Implement incremental search of users to add

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,5 +1,43 @@
 $(function(){
+  var search_list = $("#user-search-result");
+  function appendUser(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${ user.name }</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${ user.id } data-user-name=${ user.name }>追加</a>
+                </div>`
+    search_list.append(html);
+  }
+
+  function appendNoUser(user){
+    var html = `<div class="chat-group-user cleafix">
+                  <p class="chat-group-user__name">${ user }</p>
+                </div>`
+    search_list.append(html);
+  }
+
   $("#user-search-field").on("keyup", function(){
-    var input = $("#user-search-field").val();
+    var input = $(this).val();
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    .done(function(users){
+      $("#user-search-result").empty();
+      if (users.length !== 0){
+        users.forEach(function(user){
+          appendUser(user);
+          console.log();
+        });
+      }
+      else {
+        appendNoUser("一致するユーザーはいません");
+      }
+    })
+    .fail(function(){
+      alert('ユーザー検索に失敗しました');
+    })
   });
 });
